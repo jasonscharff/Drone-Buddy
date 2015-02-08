4880fa19bb5b1b282a4d69348552d0bca64bfb93
jasonscharff <jasonscharff@gmail.com>
2015-02-07 16:01:33 -0800

    Created Project

---
+++ Drone Buddy/Drone Buddy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Drone Buddy
+//
+//  Created by Jason Scharff on 2/7/15.
+//  Copyright (c) 2015 Jason Scharff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Drone Buddy/Drone Buddy/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Drone Buddy
+//
+//  Created by Jason Scharff on 2/7/15.
+//  Copyright (c) 2015 Jason Scharff. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
